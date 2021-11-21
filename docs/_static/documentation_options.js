e2a56c5236b5bde7f18b66c747a85c15da943790
Azure Pipeline <no-reply@bnl.gov>
2021-11-21 15:25:46 +0000

    Autopublish docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.11-16-g422b751',
+    VERSION: 'v0.1.11-17-g00915de',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
