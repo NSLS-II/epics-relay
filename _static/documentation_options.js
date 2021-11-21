a70518f6242ae87b9c2f98e675058026d307c73f
Stuart B. Wilkins <swilkins@bnl.gov>
2021-11-21 13:03:27 -0500

    Updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.12-1-g83afda9',
+    VERSION: 'v0.1.12-8-gdbc2db4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
