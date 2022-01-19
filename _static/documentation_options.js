d60c744754bca8ef923555fc727b5ac7a36fb7a1
Azure Pipeline <no-reply@bnl.gov>
2022-01-19 11:17:58 +0000

    Updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.13-5-gef0a5e1',
+    VERSION: 'v0.1.14',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
